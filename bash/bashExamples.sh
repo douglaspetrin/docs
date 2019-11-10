9c4e14dd2a2e2240e0e3bed48644b0d4fcdf6381
Douglas Petrin <douglas@douglaspetrin.com>
2019-11-10 13:36:14 -0300

    NEW: adds pullAllRepos method

--- bash/bashExamples.sh
+++ bash/bashExamples.sh
@@ -1,3 +1,5 @@
+# list ips from docker containers
+
 getDockerIps(){
     for docker_image in $(docker ps --format '{{.Names}}'); do
         DOCKER_IP=$(docker inspect $docker_image | grep "IPAddress.*[0-9].*" | awk '{print $2}')
@@ -7,3 +9,23 @@ getDockerIps(){
 alias dips=getDockerIps
 
 alias my_alias="source /my_folder/bashExamples.sh"
+
+
+# list repos and pull them
+
+
+#!/bin/bash
+
+
+function pullAllRepos() {
+
+for repository in $(ls $1)
+do
+        cd $1 && cd repository && git pull && cd .. && sleep 5
+
+done
+}
+
+read -p "Enter the path: " path
+
+pullAllRepos $path
